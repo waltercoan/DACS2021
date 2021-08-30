c19c5456fc10c3069805c21c9bcbc00e8f15cfdf
Walter Silvestre Coan <walter.coan@gmail.com>
2021-08-30 19:47:08 +0000

    passo 4.2 - implementação do cadastro FILHO (ITEMVENDA)

--- src/main/java/br/univille/walterdacs2021/controller/VendaController.java
+++ src/main/java/br/univille/walterdacs2021/controller/VendaController.java
@@ -9,9 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.walterdacs2021.model.ItemVenda;
+import br.univille.walterdacs2021.model.Produto;
 import br.univille.walterdacs2021.model.Venda;
+import br.univille.walterdacs2021.service.ProdutoService;
 import br.univille.walterdacs2021.service.VendaService;
 
 @Controller
@@ -20,7 +24,8 @@ public class VendaController {
     
     @Autowired
     private VendaService service;
-
+    @Autowired
+    private ProdutoService produtoService;
     @GetMapping
     public ModelAndView index(){
         List<Venda> listaVendas = service.getAllVendas();
@@ -30,14 +35,49 @@ public class VendaController {
     @GetMapping("/novo")
     public ModelAndView novo(@ModelAttribute Venda venda){
         HashMap<String,Object> dados = new HashMap<>();
-
+        List<Produto> listaProdutos = produtoService.getAllProdutos();
         dados.put("venda",venda);
+        dados.put("listaProdutos",listaProdutos);
+        dados.put("novoitemvenda", new ItemVenda());
         return new ModelAndView("venda/form",dados);
     }
 
-    @PostMapping(params = "form")
+    @PostMapping()
     public ModelAndView save(Venda venda){
+
+        venda.setValorTotal(venda.getColItemVenda().stream().reduce( 0f, (acumulador, item) -> acumulador + item.getValor(),Float::sum));
         service.save(venda);
         return new ModelAndView("redirect:/venda");
+    }
+
+    @PostMapping(params= {"insertproc"})
+    public ModelAndView insertproc(Venda venda, ItemVenda novoitemvenda) {
+
+        //Adiciona o novo item de venda na venda
+        novoitemvenda.setValor(novoitemvenda.getProduto().getPreco() - novoitemvenda.getValorDesconto());
+        venda.getColItemVenda().add(novoitemvenda);
+
+        HashMap<String,Object> dados = new HashMap<>();
+        List<Produto> listaProdutos = produtoService.getAllProdutos();
+        dados.put("venda",venda);
+        dados.put("listaProdutos",listaProdutos);
+        dados.put("novoitemvenda", new ItemVenda());
+
+        return new ModelAndView("venda/form",dados);
+    }
+
+    @PostMapping(params= {"removeproc"})
+    public ModelAndView removeproc(@RequestParam(name = "removeproc") int index, Venda venda, ItemVenda novoitemvenda) {
+
+        //Remove o item de venda da venda
+        venda.getColItemVenda().remove(index);
+
+        HashMap<String,Object> dados = new HashMap<>();
+        List<Produto> listaProdutos = produtoService.getAllProdutos();
+        dados.put("venda",venda);
+        dados.put("listaProdutos",listaProdutos);
+        dados.put("novoitemvenda", new ItemVenda());
+
+        return new ModelAndView("venda/form",dados);
     }
 }
--- src/main/java/br/univille/walterdacs2021/model/Venda.java
+++ src/main/java/br/univille/walterdacs2021/model/Venda.java
@@ -29,7 +29,7 @@ public class Venda {
     @Column(length = 15)
     private String cupom;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL,orphanRemoval=true)
     @JoinColumn(name = "venda_id")
     private List<ItemVenda> colItemVenda = new ArrayList<ItemVenda>();
     
@@ -56,6 +56,12 @@ public class Venda {
     }
     public void setCupom(String cupom) {
         this.cupom = cupom;
+    }
+    public List<ItemVenda> getColItemVenda() {
+        return colItemVenda;
+    }
+    public void setColItemVenda(List<ItemVenda> colItemVenda) {
+        this.colItemVenda = colItemVenda;
     }
 
         
