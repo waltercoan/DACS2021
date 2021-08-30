faed79922f23f229776d799d41955080bf8b972d
Walter Silvestre Coan <walter.coan@gmail.com>
2021-08-30 19:02:37 +0000

    passo 2 - correção da associação entre a classe ItemVenda e Produto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT IGNORE INTO CATEGORIA(id, nome) values(1,'Brinquedos') ;
 INSERT IGNORE INTO CATEGORIA(id, nome) values(2,'Video Games') ;
 INSERT IGNORE INTO CATEGORIA(id, nome) values(3,'Jogos de Tabuleiro') ;
+
+
+
+
