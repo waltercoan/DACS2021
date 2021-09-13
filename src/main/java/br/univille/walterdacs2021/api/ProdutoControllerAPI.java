d1e5e272b2950a20c88ad8947cbb19356fa96a2d
Walter Silvestre Coan <walter.coan@gmail.com>
2021-09-13 23:24:13 +0000

    implementacao API REST

--- src/main/java/br/univille/walterdacs2021/api/ProdutoControllerAPI.java
+++ src/main/java/br/univille/walterdacs2021/api/ProdutoControllerAPI.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.univille.walterdacs2021.model.Produto;
 import br.univille.walterdacs2021.service.ProdutoService;
+import io.swagger.models.Response;
 
 
 @RestController
@@ -22,10 +27,42 @@ public class ProdutoControllerAPI {
     @GetMapping
     public ResponseEntity<List<Produto>> getAll(){
         try{
-            return new ResponseEntity<List<Produto>>(service.getAllProdutos(), HttpStatus.OK);
+            List<Produto> listaProdutos = service.getAllProdutos();
+            return new ResponseEntity<List<Produto>>(listaProdutos, HttpStatus.OK);
         }catch (Exception ex){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getById(@PathVariable("id") Produto produto){
+        try{
+            return new ResponseEntity<Produto>(produto,HttpStatus.OK);
+        }catch (Exception ex){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @PostMapping
+    public ResponseEntity<Produto> save(@RequestBody Produto produto){
+        try{
+            produto.setId(0);
+            service.save(produto);
+            return new ResponseEntity<Produto>(produto,HttpStatus.CREATED);
+        }catch(Exception ex){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Produto> delete(@PathVariable("id") Produto produto){
+        try{
+            service.delete(produto);
+            return new ResponseEntity<Produto>(produto,HttpStatus.OK);
+        }catch(Exception ex){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
